77b6c27477d3e55f981875fea1ad21c21dcaac13
Nika <nikabylina@gmail.com>
2023-02-27 22:06:24 +0300

    add strings_01

---
+++ strings/srtings_01.py
@@ -0,0 +1,5 @@
+# Создать переменные firstname, lastname, age с соответствующими значениями
+firstname = 'Иван'
+lastname = 'Иванович'
+age = '35'
+print('Привет' + ', ' + 'меня ' + 'зовут ' + firstname + ' ' + lastname + ', ' + 'мне ' + age + ' ' + 'лет')
