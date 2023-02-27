64d4ca12a7f30c470f892c54e35e528a559e0635
Nika <nikabylina@gmail.com>
2023-02-27 21:17:57 +0300

    basics_03

--- basics_01/basics.py
+++ basics_01/basics.py
@@ -1,14 +1,13 @@
-#Определить переменные всех 4 базовых типов и вывести их на экран
+# Определить переменные всех 4 базовых типов и вывести их на экран
 
-my_int=21
+my_int = 21
 print(my_int)
 
-my_float= 21,5
-print ( my_float)
+my_float = 21, 5
+print(my_float)
 
-my_str='bdkfjb'
-print (my_str)
+my_str = 'bdkfjb'
+print(my_str)
 
 my_bool = True
 print(my_bool)
-
---
+++ basics_01/basics_03.py
@@ -0,0 +1,9 @@
+x = 18
+y = 43
+a = 22
+b = x + y + a
+print(b)
+
+s = b / 3
+print(s)
+
